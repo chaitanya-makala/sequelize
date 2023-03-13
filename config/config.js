b84a3be52f631952efcd77eef058bbae139156b0
Chaitanya Makala <chaitanya.m.1996@gmail.com>
2023-03-14 03:19:33 +0530

    changed config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize("evaluation", "root", "Password@123", {
-  host: "localhost",
+const sequelize = new Sequelize("rdsTest", "admin", "abcd1234", {
+  host: "database-1.cjrbkjkvr8eq.eu-north-1.rds.amazonaws.com",
   dialect: "mysql",
 });
 
